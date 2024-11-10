4baa9c087ca66f213fcc8da6fb5abe1a5539ee57
jaeha58 <kjh982900@gmail.com>
2024-11-11 00:00:39 +0900

    Update 2566 최댓값.c

--- 2566 최댓값.c
+++ 2566 최댓값.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
 int main(void) {
-    int i, j, l_i, l_j;
-    int max = 0;
-    for (i = 1; i <= 9; i++) {
-        for (j = 1; j <= 9;j++) {
-            int input;
-            scanf_s("%d", &input);
-            if (input > max) {
-                max = input;
-                l_i = i;
-                l_j = j;
-            }
-        }
-    }
-    printf("%d\n%d %d", max, l_i, l_j);
-    return 0;
+	int a[9][9];
+	int i, j, max = 0, max_i, max_j;
+
+	for (i = 0; i < 9; i++) {
+		for (j = 0; j < 9; j++) {
+			scanf("%d", &a[i][j]);
+
+			if (a[i][j] >= max) {
+				max = a[i][j];
+				max_i = i;
+				max_j = j;
+			}
+		}
+	}
+	printf("%d\n%d %d", max, max_i + 1, max_j + 1);
+	return 0;
 }
